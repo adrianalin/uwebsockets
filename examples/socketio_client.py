ce3f376052630863b3ca92fd7aee7e216d01e9f7
adrian.pop <pop.adrian61@gmail.com>
2019-08-11 10:00:41 +0300

    Remove self.

--- examples/socketio_client.py
+++ examples/socketio_client.py
@@ -7,11 +7,11 @@ import usocketio.client
 def hello():
     with usocketio.client.connect('http://127.0.0.1:5000/') as socketio:
         @socketio.on('message')
-        def on_message(self, message):
+        def on_message(message):
             print("message", message)
 
         @socketio.on('alert')
-        def on_alert(self, message):
+        def on_alert(message):
             print("alert", message)
 
         socketio.run_forever()
